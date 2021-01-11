e88776257c48084d61b421c694460748d41e520e
riccardo amici <amici95@gmail.com>
2021-01-11 18:17:14 +0100

    changed lineWidth

--- myscript.js
+++ myscript.js
@@ -2,9 +2,10 @@ const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.strokeStyle = '#BADASS';
+ctx.strokeStyle = '#000000';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 40;
 
 let isDrawing = false;
 let lastX = 0;
